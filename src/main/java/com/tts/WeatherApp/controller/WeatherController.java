c5a0d3fd444154d9dcdbe13f39102d1c0c88ed72
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-05-28 16:28:21 -0400

    blue background

--- src/main/java/com/tts/WeatherApp/controller/WeatherController.java
+++ src/main/java/com/tts/WeatherApp/controller/WeatherController.java
@@ -2,12 +2,16 @@ package com.tts.WeatherApp.controller;
 
 import com.tts.WeatherApp.model.Request;
 import com.tts.WeatherApp.model.Response;
+import com.tts.WeatherApp.model.ZipCode;
 import com.tts.WeatherApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class WeatherController {
@@ -26,6 +30,11 @@ public class WeatherController {
         Response data = weatherService.getForecast(request.getZipCode());
         model.addAttribute("data", data);
         return "index";
+    }
+
+    @ModelAttribute("searches")
+    public List<ZipCode> getLatestZipcodes() {
+        return weatherService.getLatestZipCodeSearches();
     }
 
 }
--- src/main/java/com/tts/WeatherApp/model/Person.java
+++ src/main/java/com/tts/WeatherApp/model/Person.java
@@ -1,4 +1,5 @@
 package com.tts.WeatherApp.model;
+import lombok.Data;
 
 import java.util.List;
 
